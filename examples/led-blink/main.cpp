6454a357028453d54cd58cf13bc1aab20bfc2111
Seiya Nuta <nuta@seiya.me>
2016-09-23 10:22:17 +0900

    fix led-blink

--- examples/led-blink/main.cpp
+++ examples/led-blink/main.cpp
@@ -1,14 +1,19 @@
 #include "baseos.h"
 #include "GPIO.h"
+#include "arch/esp8266/finfo.h"
 
+
+void loop() {
+    GPIO.write(4, 1);
+    for (volatile int i=0; i < 0x1000; i++);
+    GPIO.write(4, 0);
+    for (volatile int i=0; i < 0x500; i++);
+}
 
 void setup() {
 
+    GPIO.set_pin_mode(12, 1);
     GPIO.set_pin_mode(4, 1);
-    for (;;) {
-       GPIO.write(4, 1);
-       for (volatile int i=0; i < 0x10000; i++);
-       GPIO.write(4, 0);
-       for (volatile int i=0; i < 0x10000; i++);
-    }
+    GPIO.write(12, 1);
+    finfo->set_loop(loop);
 }
