5a35b52f759036639cede7f15437ff73c34e3ef8
Seiya Nuta <nuta@seiya.me>
2016-10-01 10:33:46 +0900

    use start_loop

--- api/init.cpp
+++ api/init.cpp
@@ -1,5 +1,6 @@
 #include <resea.h>
 #include "baseos.h"
+#include "loop.h"
 
 channel_t channel_server = 0;
 class _GPIO GPIO;
@@ -10,4 +11,5 @@ extern "C" void csapp_startup(void) {
     channel_server = connect_to_local(1);
     GPIO = _GPIO();
     setup();
+    start_loop();
 }
---
+++ api/loop.cpp
@@ -0,0 +1,8 @@
+#include "loop.h"
+#include "arch/esp8266/finfo.h"
+
+
+void start_loop() {
+
+    finfo->start_loop();
+}
