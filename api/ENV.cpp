157d6a6fb2d1a0367babe13be4ec213986fd9108
Seiya Nuta <nuta@seiya.me>
2016-11-15 12:03:04 +0900

    fix ENV::get

--- api/ENV.cpp
+++ api/ENV.cpp
@@ -4,6 +4,11 @@
 _ENV::_ENV() {
     vars.reserve(ENV_BUFFER_SIZE);
 
+    // TODO: use memset
+    char *p = vars.buffer();
+    for (size_t i = 0; i < vars.capacity(); i++, p++)
+        *p = 0;
+
     string url;
     url += Device.get_server_url();
     url += "/api/devices/";
@@ -13,6 +18,8 @@ _ENV::_ENV() {
     if (HTTP.GET(url, "", vars.buffer(), vars.capacity()) != 200) {
         Logging.errorln("failed to download envvars");
     }
+
+    vars._length = strlen(vars.buffer()); // FIXME
 }
 
 
@@ -20,6 +27,7 @@ bool _ENV::get(const char *name, char *value, size_t value_size) {
 
     size_t name_len = strlen(name);
     while (1) {
+        Logging.printlnf("envs: '%s'", vars.c_str());
         size_t offset = vars.find(name);
         if (offset < 0)
             return false; // not found
@@ -29,18 +37,20 @@ bool _ENV::get(const char *name, char *value, size_t value_size) {
         if ((offset == 0 || vars.at(prev) == ENV_SEP) && vars.at(eq) == '=') {
             // found
             const char *copy_from = vars.c_str() + eq + 1;
-            size_t sep = vars.find(ENV_SEP, offset);
+            int sep = vars.find(ENV_SEP, offset);
             size_t end = (sep > 0) ? sep : vars.length();
-            size_t copy_size = end - offset - 1;
+            size_t copy_size = end - eq - 1;
 
-            if (value_size >= copy_size) {
+            if (value_size <= copy_size + 1) {
                 Logging.errorln("envvar is too long; expand `value_size`");
                 return false;
             }
 
-            size_t n = memcpy_s(value, value_size, copy_from, copy_size);
-            value[n] = '\0';
+            memcpy_s(value, value_size, copy_from, copy_size);
+            value[copy_size] = '\0';
             return true;
+        } else {
+            offset = vars.find(ENV_SEP) + 1;
         }
     }
 }
