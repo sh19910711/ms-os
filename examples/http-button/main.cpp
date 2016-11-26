507e8d7938af88812924d0385ca09928d9fd3ae7
Seiya Nuta <nuta@seiya.me>
2016-11-26 11:07:40 +0900

    http-button: use http-button.herokuapp.com

--- examples/http-button/main.cpp
+++ examples/http-button/main.cpp
@@ -1,35 +1,27 @@
 #include <app.h>
 
-
-void post(int state) {
-    char resp[128];
-    string payload;
-    payload += "{";
-    payload += "\"id\": \"1\",";
-    payload += "\"status\": \"";
-    payload += (state)? "ON!" : "OFF!";
-    payload += "\"}";
-
-    string headers;
-    headers += "Content-Type: application/json\r\n";
-
-    HTTP.POST(CONFIG_SERVER_URL,
-              payload.c_str(), payload.length(), headers, &resp, 512);
-}
-
-
-int state;
-
+static char device_name[32];
+static int state = 0;
 void setup() {
-    state = GPIO_LOW;
-
+    ENV.get("device_name", (char *) &device_name, sizeof(device_name));
     GPIO.set_pin_mode(BUTTON_GPIO_PIN, GPIO_INPUT_PIN);
-    post(0);
 
     Timer.set_interval(POLLING_INTERVAL, []() {
         int new_state = GPIO.read(BUTTON_GPIO_PIN);
         if (new_state != state) {
-            post(new_state);
+/*
+            string url;
+            url.from_fmt("%s%s?button=%s", HTTP_BUTTON_SERVER_URL,
+                         (state) ? "push" : "pop", "?button=", device_name);
+            HTTP.POST(url);
+*/
+            char resp[8];
+            string url;
+            url += HTTP_BUTTON_SERVER_URL;
+            url += (new_state) ? "push" : "pop";
+            url += "?button=";
+            url += device_name;
+            HTTP.POST(url, "", 0, "", &resp, sizeof(resp));
             state = new_state;
         }
     });
