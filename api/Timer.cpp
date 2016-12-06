104d97e468dbd123f5b0aeb382dece3a92be86e7
Seiya Nuta <nuta@seiya.me>
2016-12-06 12:04:25 +0900

    fix Timer

--- api/Timer.cpp
+++ api/Timer.cpp
@@ -16,12 +16,12 @@ _Timer::_Timer() {
 void _Timer::set_interval(int ms, void (*callback)()) {
 
     result_t r;
-    call_timer_set_interval(timer_server, app_channel, ms, (uintmax_t) callback, &r);
+    call_timer_set_interval(timer_server, transfer_to(app_channel), ms, (uintmax_t) callback, &r);
 }
 
 
 void _Timer::set_oneshot(int ms, void (*callback)()) {
 
     result_t r;
-    call_timer_set_oneshot(timer_server, app_channel, ms, (uintmax_t) callback, &r);
+    call_timer_set_oneshot(timer_server, transfer_to(app_channel), ms, (uintmax_t) callback, &r);
 }
